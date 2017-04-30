46ed13a13664401ec90c94d5e0e891e85f254361
lebedeanastasija <lebedeanastasija@yandex.by>
2017-05-01 00:22:24 +0300

    Add word and word topic models

---
+++ database/models/wordTopics/index.js
@@ -0,0 +1,22 @@
+const sequelize = require('../../index');
+const Sequelize = require('sequelize');
+const Word = require('../words');
+
+const WordTopic = sequelize.define('word_topic', {
+    id: {
+        type: Sequelize.INTEGER,
+        field: 'word_topic_id',
+        primaryKey: true
+    },
+    name: {
+        type: Sequelize.TEXT
+    }
+}, {
+    freezeTableName: true,
+    createdAt: false,
+    updatedAt: false
+});
+
+WordTopic.hasMany(Word, {foreignKey: 'word_topic_id'});
+
+module.exports = WordTopic;
